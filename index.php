c56a23e206d0c55217f62d6edbf9eec153ed0958
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-05-29 16:34:00 -0600

    added starter index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>I Completed the Prework</title>
+	</head>
+	<Body>
+		<p>I completed the prework!</p>
+	</Body>
+</html>
